82cfbc7624621ad80dab5ec8964583e7bcd8803a
ebrezine <elias.brezine@gmail.com>
2023-03-13 15:41:24 -0700

    3/13/23

---
+++ Untitled-1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void Main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        n*=60; 
+        System.out.println(n);
+    }
+    
+}
